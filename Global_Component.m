73d73c5cb4ee5b374e1461046949b40d15a03feb
Xiao-Yang Liu <xl2427@columbia.edu>
2018-09-26 23:42:24 +0800

    reupdate

---
+++ Global_Component.m
@@ -0,0 +1,22 @@
+%%%Global Component
+%initialization
+delta = ;
+epsilon = 2*log(1/delta);
+T = ;
+L = ;
+beta = ;
+m = nu;
+n = ni;
+
+sigma = (L^2*sqrt(64*iter*log(1/delta)))/epsilon;
+v = zeros(1*n);
+lamda = 0;
+for t = 1:T
+    W = zeros(n*n);
+    lamda1 = lamda + sqrt(sigma*log(n/delta))*n^(1/4);
+    for i = 1:m
+       W = W + %local(i,v,lamda,T,t,L)% 
+    end
+    W = W + normrnd(0,sigma^2);
+    (v,lamda) = eig(W);
+end
